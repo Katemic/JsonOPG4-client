f61f7bbea65eff4a6a105978af51e1cbe026980c
Katrine <katrinemichala@gmail.com>
2024-09-29 14:27:57 +0200

    Add project files.

---
+++ JsonOPG/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonOPG
+{
+    public class Command
+    {
+        public string Method { get; set; }  
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+
+        public Command()
+        {
+            
+        }
+
+
+        public Command(string method, int number1, int number2)
+        {
+            Method = method;
+            Number1 = number1;
+            Number2 = number2;
+
+        }
+
+    }
+}
---
+++ JsonOPG/Program.cs
@@ -0,0 +1,56 @@
+﻿using JsonOPG;
+using System.Net.Sockets;
+using System.Text.Json;
+
+Console.WriteLine("This is the TCP Client");
+
+bool keepSending = true;
+
+TcpClient socket = new TcpClient("127.0.0.1", 7);
+
+
+NetworkStream ns = socket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+while (keepSending)
+{
+
+    Console.WriteLine("Pick a method");
+    string message = Console.ReadLine();
+    if (message.ToLower() == "stop")
+    {
+        writer.WriteLine(message);
+        keepSending = false;
+    }
+    else
+    {
+        Console.WriteLine("Enter number 1");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number 2");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Command command = new Command(message, number1, number2);
+
+        string jsonMessage = JsonSerializer.Serialize(command);
+
+        writer.WriteLine(jsonMessage);
+    }
+
+
+
+    writer.Flush();
+
+    string response = reader.ReadLine();
+
+
+
+    Console.WriteLine("Server respons: " + response);
+
+    if (message.ToLower() == "stop")
+    {
+        keepSending = false;
+    }
+
+
+}
